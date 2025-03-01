b037db9145f4add2b417b8855c29437e29accd5e
Leon San José Larsson <54620188+leonlarsson@users.noreply.github.com>
2025-03-01 21:51:51 +0100

    disable timestamps in Stats model and update stats table structure

--- app/Models/Stats.php
+++ app/Models/Stats.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Stats extends Model
 {
-    //
+    public $timestamps = false;
 }
--- database/migrations/2025_02_26_180748_create_stats_table.php
+++ database/migrations/2025_02_26_180748_create_stats_table.php
@@ -12,7 +12,8 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('stats', function (Blueprint $table) {
-            $table->enum('name', ['total_unboxes_all', 'total_unboxes_coverts'])->primary();
+            $table->id();
+            $table->string('name')->unique();
             $table->integer('value')->default(0);
         });
     }
